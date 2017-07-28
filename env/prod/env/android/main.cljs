7bf1ed2eedaafffa11d839054b53a5b2b4fc4efc
Mike Fikes <mfikes@orion.local>
2017-07-28 15:09:12 -0400

    first commit

---
+++ env/prod/env/android/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.android.main
+  (:require [use-native-code.android.core :as core]))
+
+ (core/init)
+
+
---
+++ test/use_native_code/core_test.clj
@@ -0,0 +1,7 @@
+(ns use-native-code.core-test
+  (:require [clojure.test :refer :all]
+            [use-native-code.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
